929f2d2bcedbf974af0d698dd8554ac4846f316a
Nitin Bhardwaj Komaravolu <knbdtu@hotmail.com>
2016-07-28 23:07:16 +0530

    Updated testcase.php

--- testcase.cpp
+++ testcase.cpp
@@ -4,14 +4,18 @@ using namespace std;
 #define mp make_pair
 #define pb push_back
 #define mt make_tuple
-#define LD long double
+#define LD double
 #define gc getchar_unlocked
 #define pc putchar_unlocked
-#define MOD 1000000007
-#define MAXN 100005
-#define bitcount _builtin_popcount
+#define MOD 1000000007LL
+#define MAXN 123456
+#define bitcount __builtin_popcount
 #define INF 2000000000
 #define EPS 1e-9
+#define PI 3.14159265359
+#define DEBUG 1
+#define read(X) scanf("%lld",&X)
+#define write(X) printf("%lld\n",&X)
 
 template<typename T>T absll(T X)
 {
@@ -21,31 +25,105 @@ template<typename T>T absll(T X)
         return X;
 }
 
+LL modPow(LL A,LL B,LL C)
+{
+	LL X=1LL;
+	while(B)
+	{
+		if(B&1)
+		{
+			X=(X*A)%C;
+		}
+		A=(A*A)%C;
+		B/=2LL;
+	}
+	return X;
+}
+
+vector<vector<LL> > hashe(26,vector<LL>(1234567,0LL));
+
 int main()
 {
-	int N,M,C;
-	scanf("%d %d %d",&N,&M,&C);
-	vector<LL> LH(1001,0);
-	vector<LL> UH(1001,0);
-	vector<LL> S(1001,0LL);
-	int X;
-	for(int i=0;i<N;i++)
+	LL sol=0LL;
+	string str;
+
+	cin>>str;
+
+
+	for(int i=0;i<str.length();i++)
 	{
-		scanf("%d",&X);
-		UH[X]+=1;
+        for(int j=0;j<26;j++)
+        {
+            if(i==0)
+            {
+                if(j==(str[i]-'a'))
+                {
+                    hashe[j][i]=1;
+                }
+                else
+                {
+                    hashe[j][i]=0;
+                }
+            }
+            else
+            {
+                if(j==(str[i]-'a'))
+                {
+                    hashe[j][i]=hashe[j][i-1]+1;
+                }
+                else
+                {
+                    hashe[j][i]=hashe[j][i-1];
+                }
+            }
+        }
 	}
-	
-	for(int i=0;i<M;i++)
+	//Calculating solo pairs
+
+	for(int i=0;i<26;i++)
 	{
-		scanf("%d",&X);
-		LH[X]+=1;
+		LL term=hashe[i][str.length()-1];
+
+		if(term>=4)
+		{
+			LL cont=term;
+			term-=1;
+			cont*=term;
+			cont%=MOD;
+			term-=1;
+			cont*=term;
+			cont%=MOD;
+			term-=1;
+			cont*=term;
+			cont%=MOD;
+			cont*=modPow(24LL,MOD-2,MOD);
+			cont%=MOD;
+
+			if(sol>=MOD)
+			{
+				sol%=MOD;
+			}
+		}
 	}
-	
-	for(int i=1;i<1001;i++)
+	cout<<sol<<endl;
+	//Calculate pairwise-letters
+	vector<string> P;
+
+	for(char i='a';i<='z';i++)
 	{
-		S[i]=LH[i]*UH[i];
+		for(char j=i+1;j<='z';j++)
+		{
+			string tmp="";
+			tmp+=i;
+			tmp+=j;
+			P.pb(tmp);
+		}
 	}
-	
-	
+
+	for(int i=0;i<P.size();i++)
+	{
+
+	}
+
 	return 0;
 }
